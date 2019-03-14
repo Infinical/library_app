e3455a9f73140c7153f33c241c0ed65e6ffa2f82
Infinical <inficalvin@gmail.com>
2019-03-14 20:11:07 +0300

    Create,Show and List Books

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,6 +10,14 @@ class BooksController < ApplicationController
         redirect_to @book
     end
 
+    def show
+        @book = Book.find(params[:id])
+    end
+
+    def index
+        @books = Book.all
+    end
+
     private
     def book_params
         params.require(:book).permit(:book_name,:book_ISBN,:book_publisher,:book_year)
