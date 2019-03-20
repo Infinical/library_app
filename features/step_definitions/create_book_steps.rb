2b0664b93530c12860c29cf9b587b23cac66d8bf
Infinical <inficalvin@gmail.com>
2019-03-20 15:42:05 +0300

    Remove some steps

--- features/step_definitions/create_book_steps.rb
+++ features/step_definitions/create_book_steps.rb
@@ -5,11 +5,3 @@ end
 Then('I expect to see {string}') do |string|
   expect(page).to have_content string
 end
-
-Then('I fill {string} with {string}') do |_string, _string2|
-  pending # Write code here that turns the phrase above into concrete actions
-end
-
-Then('I click on {string}') do |_string|
-  pending # Write code here that turns the phrase above into concrete actions
-end
