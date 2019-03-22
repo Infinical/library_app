f908f213a0b4bc2410e5852d01452f1a2f842f8e
Infinical <inficalvin@gmail.com>
2019-03-22 12:08:41 +0300

    Correcting error

--- features/step_definitions/create_book_steps.rb
+++ features/step_definitions/create_book_steps.rb
@@ -6,12 +6,12 @@ Then('I expect to see {string}') do |string|
   expect(page).to have_content string
 end
 
-Then("I fill {string} with {string}") do |string, string2|
-    fill_in "Book title",	with: "React" 
-    fill_in "Book isbn",	with: "12345678" 
-    fill_in "Year published",	with: "2017" 
+Then('I fill {string} with {string}') do |_string, _string2|
+  fill_in 'Book title',	with: 'React'
+  fill_in 'Book isbn',	with: '12345678'
+  fill_in 'Year published',	with: '2017'
 end
 
-Then("I click on {string}") do |string|
+Then('I click on {string}') do |_string|
   click_button 'Save Book'
 end
