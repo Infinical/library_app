5614cb53b0a2d6637fb08dcbe121cfe58140af52
Infinical <inficalvin@gmail.com>
2019-03-22 13:16:56 +0300

    Adding flash message

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,14 @@
 # Book Controller
 class BooksController < ApplicationController
-  def create; end
+  def create
+    @book = Book.new(params[:book])
+
+    if @book.save
+      flash[:success] = "Book Successfully Created"
+    else
+      render 'new'
+    end
+  end
+
+  
 end
