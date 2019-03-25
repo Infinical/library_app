42a0059e058b0f1cbb7e6cd2a30f7c8b6e059287
Infinical <inficalvin@gmail.com>
2019-03-25 10:57:03 +0300

    Syntax errors

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,16 +1,16 @@
+# Book Controller
 class BooksController < ApplicationController
-    def new
-    end
+  def new; end
 
-    def create
-        @book = Book.new(book_params)
+  def create
+    @book = Book.new(book_params)
 
-        @book.save
-       
-    end
+    @book.save
+  end
 
-    private
-    def book_params
-                params.require(:book).permit(:title, :isbn, :year)
-    end
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :isbn, :year)
+  end
 end
